8f9110e1294a95ba505a639c3ce780410776a8f1
kenshin <73159641+QAQdev@users.noreply.github.com>
2022-12-01 19:40:07 +0800

    Update lineGraph.js

--- src/lineGraph.js
+++ src/lineGraph.js
@@ -5,12 +5,56 @@ import { Card } from '@douyinfe/semi-ui';
 import { useState, useEffect } from 'react';
 
 
-export default function Graph() {
+export default function LineGraph(props) {
   // axios获取后端数据
+  var timeline;
+  var ydata;
+  if(props.timeline)
+  {
+    timeline = props.timeline
+  }
+  else 
+  {
+    timeline = ['2022/10/1', '2022/10/2', '2022/10/3', 
+        '2022/10/4', '2022/10/5', '2022/10/6', '2022/10/7',
+        '2021/10/1', '2021/10/2', '2021/10/3', 
+        '2021/10/4', '2021/10/5', '2021/10/6', '2021/10/7',
+        '2022/10/1', '2022/10/2', '2022/10/3', 
+        '2022/10/4', '2022/10/5', '2022/10/6', '2022/10/7',
+        '2021/10/1', '2021/10/2', '2021/10/3', 
+        '2021/10/4', '2021/10/5', '2021/10/6', '2021/10/7',
+        '2022/10/1', '2022/10/2', '2022/10/3', 
+        '2022/10/4', '2022/10/5', '2022/10/6', '2022/10/7',
+        '2021/10/1', '2021/10/2', '2021/10/3', 
+        '2021/10/4', '2021/10/5', '2021/10/6', '2021/10/7'
+      ]
+  }
+
+  if(props.ydata)
+  {
+    ydata = props.ydata
+  }
+  else 
+  {
+    ydata = [820, 932, 901, 934, 1290, 1330, 1320,
+      820, 932, 901, 934, 1290, 1330, 1320,
+      820, 932, 901, 934, 1290, 1330, 1320,
+      820, 932, 901, 934, 1290, 1330, 1320,
+      820, 932, 901, 934, 1290, 1330, 1320,
+      820, 932, 901, 934, 1290, 1330, 1320
+    ]
+  }
+
   const [data, setData] = useState({
-    timeline: ['2022/10/1', '2022/10/2', '2022/10/3', '2022/10/4', '2022/10/5', '2022/10/6', '2022/10/7'],
-    ydata: [820, 932, 901, 934, 1290, 1330, 1320]
+    timeline: timeline,
+    ydata: ydata
   })
+
+  var text = "defaut name"
+  if(props.repo_name)
+  {
+    text = props.repo_name
+  }
 
   const options = {
     tooltip: {
@@ -19,9 +63,10 @@ export default function Graph() {
         return [pt[0], '10%'];
       }
     },
+
     title: {
       left: 'center',
-      text: 'Commit'
+      text: text
     },
     toolbox: {
       feature: {
